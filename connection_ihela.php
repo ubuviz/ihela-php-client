8e2289e2390b058134788eff78f90c9cd7ec10d1
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2020-08-11 11:37:06 +0200

    Spaces

--- connection_ihela.php
+++ connection_ihela.php
@@ -8,58 +8,58 @@ class IhelaConnection
     protected $_api_url;
 
     public function __construct($client_id, $client_secret, $test) {
-		if ($test) {
-			$this->_api_url = 'https://testgate.ihela.online';
-		} else {
-			$this->_api_url = 'https://api.ihela.online';
-		}
+        if ($test) {
+            $this->_api_url = 'https://testgate.ihela.online';
+        } else {
+            $this->_api_url = 'https://api.ihela.online';
+        }
 
-		// Here we get the token for all calls
-	    $this->setClient();
-	    $this->setToken($client_id, $client_secret);
-	}
+        // Here we get the token for all calls
+        $this->setClient();
+        $this->setToken($client_id, $client_secret);
+    }
 
-	protected function setClient() {
-    	$this->_client = new Client('base_uri' => $this->_api_url);
+    protected function setClient() {
+        $this->_client = new Client('base_uri' => $this->_api_url);
     }
 
     protected function getClient() {
-    	return $this->_client;
+        return $this->_client;
     }
 
     protected function setToken($client_id, $client_secret) {
-		$client = $this->getClient();
+        $client = $this->getClient();
 
-		$headers = array('Content-Type' => 'application/json')
-		$res = $client->post("oAuth2/token", [
-		    'auth' => [$client_id, $client_secret], 'headers' => $headers, 'json': ['grant_type' => 'client_credentials']
-		]);
+        $headers = array('Content-Type' => 'application/json')
+        $res = $client->post("oAuth2/token", [
+            'auth' => [$client_id, $client_secret], 'headers' => $headers, 'json': ['grant_type' => 'client_credentials']
+        ]);
 
-		// echo $res->getStatusCode();
-		// "200"
-		// echo $res->getHeader('content-type')[0];
-		// 'application/json; charset=utf8'
-		$body = $res->getBody();
+        // echo $res->getStatusCode();
+        // "200"
+        // echo $res->getHeader('content-type')[0];
+        // 'application/json; charset=utf8'
+        $body = $res->getBody();
 
-		$this->_token = $body["token"];
-	}
+        $this->_token = $body["token"];
+    }
 
-	protected function getToken() {
-		return $this->_token;
-	}
+    protected function getToken() {
+        return $this->_token;
+    }
 
-	public function initBill($amount, $merchant_reference, $description, $user, $redirect_uri=null) {
-		$client = $this->getClient();
+    public function initBill($amount, $merchant_reference, $description, $user, $redirect_uri=null) {
+        $client = $this->getClient();
 
-	    $data = array('amount' => $amount, 'merchant_reference' => $merchant_reference, 'description', $description, 'user': $user, "redirect_uri", $redirect_uri);
-	    $url = "api/v1/payments/bill/init";
-	    $headers = array('Content-Type' => 'application/json','Authorization'=> "Bearer $this->getToken()");
+        $data = array('amount' => $amount, 'merchant_reference' => $merchant_reference, 'description', $description, 'user': $user, "redirect_uri", $redirect_uri);
+        $url = "api/v1/payments/bill/init";
+        $headers = array('Content-Type' => 'application/json','Authorization'=> "Bearer $this->getToken()");
 
-	    $response = $client->post($url, $headers, [
-	    	'headers' => $headers,
-    		'json' => $data
-		]);
-	}
+        $response = $client->post($url, $headers, [
+            'headers' => $headers,
+            'json' => $data
+        ]);
+    }
 
 
 }
